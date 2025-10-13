4542698d4882904410e81f6084478da57c7d371e
siddu0426 <msvssiddhartha@gmail.com>
2025-10-13 12:56:10 +0530

    build log error solving

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PROJECTS } from '../constants';
+import { PROJECTS } from '../constants.jsx';
 import { GithubIcon, ExternalLinkIcon } from './icons/SocialIcons';
 
 const Projects = () => {
