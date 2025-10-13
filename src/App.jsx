b8026b6d5be1229c54f2b5d74d5d2e36f695094f
siddu0426 <msvssiddhartha@gmail.com>
2025-10-13 13:06:04 +0530

    resolved issue of deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
-import Projects from './components/projects';
+import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Skills from './components/Skills';
